d54b2154ca962c826db4e3dfebec21332fc00e06
Peter Kos <pkos91@icloud.com>
2016-08-04 18:24:01 -0400

    Duplicate user check on signup, improved networking
    Closures, closures everywhere.

--- iLabeacon/SignupViewController.swift
+++ iLabeacon/SignupViewController.swift
@@ -21,36 +21,38 @@ class SignupViewController: UIViewController {
 		let name = nameField.text!
 		
 		// If name already exists, show an alert controller informing the user.
-		guard checkIfNameExists(name) else {
+		checkIfNameExists(name, completion: { (exists) in
+			guard exists else {
+				
+				let alertController = UIAlertController(title: "Name Exists", message: "Name already exists. Please choose another.", preferredStyle: .Alert)
+				
+				let continueAction = UIAlertAction(title: "Ok", style: .Default, handler: { action in
+					self.nameField.text! = ""
+					print("User continued.")
+				})
+				
+				alertController.addAction(continueAction)
+				self.presentViewController(alertController, animated: true, completion: nil)
+				
+				return
+			}
 			
-			let alertController = UIAlertController(title: "Name Exists", message: "Name already exists. Please choose another.", preferredStyle: .Alert)
+			// Adds new user and posts notification to MainTBVC
+			//		NSNotificationCenter.defaultCenter().postNotificationName("setLocalUser", object: nil)
 			
-			let continueAction = UIAlertAction(title: "Ok", style: .Default, handler: { action in
-				self.nameField.text! = ""
-				print("User continued.")
-			})
+			self.saveUser(name)
 			
-			alertController.addAction(continueAction)
-			self.presentViewController(alertController, animated: true, completion: nil)
+			// Sets launch key to false
+			let userDefaults = NSUserDefaults.standardUserDefaults()
+			userDefaults.setBool(true, forKey: "hasLaunchedBefore")
 			
-			return
-		}
-		
-		// Adds new user and posts notification to MainTBVC
-//		NSNotificationCenter.defaultCenter().postNotificationName("setLocalUser", object: nil)
-		
-		saveUser(name)
-		
-		// Sets launch key to false
-		let userDefaults = NSUserDefaults.standardUserDefaults()
-		userDefaults.setBool(true, forKey: "hasLaunchedBefore")
-		
-		// Instnatiates main view
-		let storyboard = UIStoryboard(name: "Main", bundle: nil)
-		let mainVC = storyboard.instantiateViewControllerWithIdentifier("MainNavVC")
-		self.presentViewController(mainVC, animated: true) {
-			NSNotificationCenter.defaultCenter().postNotificationName("NewUser", object: nil, userInfo: ["name": name])
-		}
+			// Instnatiates main view
+			let storyboard = UIStoryboard(name: "Main", bundle: nil)
+			let mainVC = storyboard.instantiateViewControllerWithIdentifier("MainNavVC")
+			self.presentViewController(mainVC, animated: true) {
+				NSNotificationCenter.defaultCenter().postNotificationName("NewUser", object: nil, userInfo: ["name": name])
+			}
+		})
 		
 	}
 	
@@ -66,20 +68,26 @@ class SignupViewController: UIViewController {
 	// MARK: - Firebase FUNctions
 	
 	// Fetch user, check if they exist. Easy enough, right?
-	func checkIfNameExists(name: String) -> Bool {
+	func checkIfNameExists(name: String, completion: (exists: Bool) -> Void) {
 		
-		var serverUserName = ""
+		var nameExists = false {
+			didSet {
+				completion(exists: nameExists)
+			}
+		}
 		
-		usersReference.child(name).observeSingleEventOfType(.Value, withBlock: { snapshot in
-			serverUserName = snapshot.key
+		usersReference.observeSingleEventOfType(.Value, withBlock: { snapshot in
+			if (snapshot.hasChild(name)) {
+				print("false, duplicate")
+				nameExists = false
+			} else {
+				print("true, unique")
+				nameExists = true
+			}
 		}) { error in
 			print("error")
 		}
 		
-		print("Name \(name) exists: ")
-		print("server name: \(serverUserName)")
-		
-		return (serverUserName != name) ? true : false
 	}
 	
 	func saveUser(name: String) {
