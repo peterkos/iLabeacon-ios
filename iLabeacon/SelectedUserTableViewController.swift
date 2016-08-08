84169eeb3770e07480b5c2e4b813eea0136e694a
Peter Kos <pkos91@icloud.com>
2016-08-08 01:03:05 -0400

    Removed per-user Beacon data
    Will move to localUser, in separate tabbar

--- iLabeacon/SelectedUserTableViewController.swift
+++ iLabeacon/SelectedUserTableViewController.swift
@@ -16,15 +16,6 @@ class SelectedUserTableViewController: UITableViewController {
 	@IBOutlet weak var userDateLastInCell: UITableViewCell!
 	@IBOutlet weak var userDateLastOutCell: UITableViewCell!
 	
-	// Location (Beacon)
-	@IBOutlet weak var beaconUUIDCell: UITableViewCell!
-	@IBOutlet weak var beaconRSSICell: UITableViewCell!
-	@IBOutlet weak var beaconMajorCell: UITableViewCell!
-	@IBOutlet weak var beaconMinorCell: UITableViewCell!
-	@IBOutlet weak var beaconProximityCell: UITableViewCell!
-	@IBOutlet weak var beaconAccuracyCell: UITableViewCell!
-	
-	
 	var user: User? = nil
 	var beacon: Beacon? = nil
 	
@@ -44,14 +35,6 @@ class SelectedUserTableViewController: UITableViewController {
 		userIsInCell.detailTextLabel!.text = isInToEnglish()
 		userDateLastInCell.detailTextLabel!.text = dateFormatter.stringFromDate(user!.dateLastIn)
 		userDateLastOutCell.detailTextLabel!.text = dateFormatter.stringFromDate(user!.dateLastOut)
-		
-		// Beacon
-		beaconUUIDCell.detailTextLabel!.text = beacon?.uuid?.description
-		beaconRSSICell.detailTextLabel!.text = beacon?.rssi?.description
-		beaconMajorCell.detailTextLabel!.text = beacon?.major?.description
-		beaconMinorCell.detailTextLabel!.text = beacon?.minor?.description
-		beaconProximityCell.detailTextLabel!.text = beacon?.proximity?.description
-		beaconAccuracyCell.detailTextLabel!.text = beacon?.accuracy?.description
 		
 		// Sets nav bar title to usernmae
 		self.title = user?.name
