93abed037c56d46b1df142f613186b8cf38a12d8
Peter Kos <pkos91@icloud.com>
2016-08-08 02:14:34 -0400

    Framework for user settings

--- iLabeacon/LocalUserSettingsTableViewController.swift
+++ iLabeacon/LocalUserSettingsTableViewController.swift
@@ -6,8 +6,38 @@
 //  Copyright © 2016 Peter Kos. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class LocalUserSettingsTableViewController: UITableViewController {
+	
+	
+	@IBOutlet weak var uuidCell: UITableViewCell!
+	@IBOutlet weak var rssiCell: UITableViewCell!
+	@IBOutlet weak var majorCell: UITableViewCell!
+	@IBOutlet weak var minorCell: UITableViewCell!
+	@IBOutlet weak var proximityCell: UITableViewCell!
+	@IBOutlet weak var isInCell: UITableViewCell!
+	
+	var beacon: Beacon? = nil
+	var user: User? = nil
+	
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		uuidCell.detailTextLabel!.text = beacon?.uuid?.description
+		rssiCell.detailTextLabel!.text = beacon?.rssi?.description
+		majorCell.detailTextLabel!.text = beacon?.major?.description
+		proximityCell.detailTextLabel!.text = beacon?.proximity?.description
+		isInCell.detailTextLabel!.text = user?.isIn
+		
+	}
+	
+	override func viewDidLoad() {
+		// Because IB is being stubborn
+		self.title = "Local uesr settings"
+	}
+	
+	
+	
 
 }
