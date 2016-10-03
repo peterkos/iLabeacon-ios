33b6db8bbb313060b0785da20cc1350822a7ee15
Peter Kos <pkos91@icloud.com>
2016-10-03 19:53:41 -0400

    Added PermissionScope

--- Pods/Target Support Files/Pods-iLabeaconUITests/Pods-iLabeaconUITests-frameworks.sh
+++ Pods/Target Support Files/Pods-iLabeaconUITests/Pods-iLabeaconUITests-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/PermissionScope/PermissionScope.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SVProgressHUD/SVProgressHUD.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/PermissionScope/PermissionScope.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SVProgressHUD/SVProgressHUD.framework"
 fi
